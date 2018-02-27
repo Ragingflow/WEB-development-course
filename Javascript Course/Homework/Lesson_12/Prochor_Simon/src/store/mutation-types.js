674ff90af9bfb164cd50d7a2818b0fa6a18dc14f
prochor <prochorz@gmail.com>
2018-02-27 23:13:49 +0200

    edit HW12

--- Javascript Course/Homework/Lesson_12/Prochor_Simon/src/store/mutation-types.js
+++ Javascript Course/Homework/Lesson_12/Prochor_Simon/src/store/mutation-types.js
@@ -1,7 +1,6 @@
 export const SEARCH_RESULT = 'SEARCH_RESULT';
 
 export const SET_CART = 'SET_CART';
-export const REMOVE_CART = 'REMOVE_CART';
 
 
 export const SIGN_IN = 'SIGN_IN';
--- Javascript Course/Homework/Lesson_12/Prochor_Simon/src/store/mutations.js
+++ Javascript Course/Homework/Lesson_12/Prochor_Simon/src/store/mutations.js
@@ -2,11 +2,7 @@ import * as types from './mutation-types';
 
 export const mutations = {
     [types.SET_CART](state, books_payload){
-        //state.cart.push( books_payload );
         state.cart = books_payload;
-    },
-    [types.REMOVE_CART](state, index){
-        state.cart.splice( index, 1 );
     },
     [types.SEARCH_RESULT](state, a){
         state.searchResult =  {
