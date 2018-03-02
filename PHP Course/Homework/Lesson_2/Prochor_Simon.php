e99576ff7dc409bca6fe2f58acdf824702dbb74e
prochor <prochorz@gmail.com>
2018-03-02 10:44:50 +0200

    fix PHP HW2 Prochor_Simon

--- PHP Course/Homework/Lesson_2/Prochor_Simon.php
+++ PHP Course/Homework/Lesson_2/Prochor_Simon.php
@@ -1,15 +1,19 @@
 <?php
-$string = 'a(bcdefghijkl(mno)p)q';
+$string = 'The ((quick (brown) (fox) jumps over the lazy) dog)';
 
 function reversString( $str ) {
-    $regex = '/\([^\(\)]*\)/';
-    preg_match( $regex, $str, $matches);
+    $regexFind = '/\(([^\(\)]*)\)/';
+    preg_match_all( $regexFind, $str, $matches);
 
-    if(isset($matches[0])) {
-        $reverseStr = str_replace(['(',')'], '', $matches[0]);
-        $reverseStr = strrev($reverseStr);
+    if( !empty($matches[0])) {
+        $reverseArrayStr = [];
+        foreach( $matches[1] as $v){
+            $reverseArrayStr[] = strrev($v);
+        }
+        $newStr = str_replace($matches[0], $reverseArrayStr, $str);
 
-        return reversString( preg_replace($regex, $reverseStr, $str) );
+
+        return reversString( $newStr  );
     }
 
 
