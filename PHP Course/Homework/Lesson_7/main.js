e51d7843957eee460f257fcb5fec11eed6e67e6c
NickGorbunoff <nick.gorbunoff2010@gmail.com>
2018-04-11 22:53:06 +0300

    Created registration_form

---
+++ PHP Course/Homework/Lesson_7/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+	$('#form').validate({
+		rules: {
+			name: {
+				required:true,
+				minlength: 2
+			},
+			lastname: {
+				required:true,
+				minlength: 2
+			},
+			email: {
+				required:true,
+				email:true
+			}
+		},
+		messages: {
+			name: {
+				required:"Введите Ваше Имя",
+				minlength: "Используйте не менее двух символов"
+			},
+			lastname: {
+				required:"Введите Вашу Фамилию",
+				minlength: "Используйте не менее двух символов"
+			},
+			email: {
+				required:"Введите Ваш email",
+				email:"Используйте правильный формат email"
+			}
+		},
+		submitHandler: function(){
+			$.ajax({
+				type: "POST",
+				url: "registration_form.php",
+				data: $('#form').serialize(),
+				success: function(data) {
+					$('.messages').html(data);
+					$('.user_data').val('');
+				}
+			})
+		}
+	});
+});
