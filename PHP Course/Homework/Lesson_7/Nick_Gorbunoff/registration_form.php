f63e1e39a0aab79c3fd8d893fb22eb7c81d3744e
NickGorbunoff <nick.gorbunoff2010@gmail.com>
2018-04-18 00:05:56 +0300

    Rewrite config php files

--- PHP Course/Homework/Lesson_7/Nick_Gorbunoff/registration_form.php
+++ PHP Course/Homework/Lesson_7/Nick_Gorbunoff/registration_form.php
@@ -1,165 +1,41 @@
 <?php
-
-	class Config {
-
-		private $name = "";
-		private $lastname = "";
-		private $email = "";
-		private $ticket = "";
-		private $successMessage = "";
-		private $errorMessage = "";
 	
-		private function clean($value = "") {
-			$value = trim($value);
-			$value = stripslashes($value);
-			$value = strip_tags($value);
-			$value = htmlspecialchars($value);
-			return $value;
-		}
+	require_once('configDB.php');
 
-		private function check_length($value = "", $min, $max) {
-			$result = (mb_strlen($value) < $min || mb_strlen($value) > $max);
-			return !$result;
-		}
+	require_once('validator.php');
 
-		public function config_reg($selectSaveWay) {
+	spl_autoload_register(function ($class) {
+		include $class . '.php';
+	});
 
-			if (($_POST) == true) {
+	$userData = $_POST;
 
-				$name = $this->clean($_POST["name"]);
-				$lastname = $this->clean($_POST["lastname"]);
-				$email = $this->clean($_POST["email"]);
-				$ticket = $this->clean($_POST["ticket"]);
+	$validate = new Valid();
+	$validate->config_valid($userData); 
+	
+	class Config{
 
-				$email_validate = filter_var($email, FILTER_VALIDATE_EMAIL);
+		public function config_reg($selectSaveWay){
 
-				if(!empty($name) && !empty($lastname) && !empty($ticket)) {
+			$userData = $_POST;
 
-					if($this->check_length($name, 2, 15) && $this->check_length($lastname, 2, 20) && $email_validate) {
+			switch($selectSaveWay) {
 
-						switch($selectSaveWay) {
+				case 'text':
+				$txt = new SaveToTxt();
+				$txt->text_save($userData);
+				break;
 
-							case 'text':
-							$txt = new SaveToTxt();
-							$txt->text_save($name, $lastname, $email, $ticket);
-							break;
+				case 'db':
+				$db = new SaveToDB();
+				$db->db_save($userData);
+				break;
 
-							case 'db':
-							$db = new SaveToDB();
-							$db->db_save($name, $lastname, $email, $ticket);
-							break;
+				case 'xml':
+				$xml = new SaveToXML();
+				$xml->xml_save($userData);
+				break;
 
-							case 'xml':
-							$xml = new SaveToXML();
-							$xml->xml_save($name, $lastname, $email, $ticket);
-							break;
-
-						}
-						
-					} else {
-						echo $errorMessage = "Введенные данные некорректные";
-						exit();
-					}
-
-				} else {
-						echo $errorMessage = "Заполните пустые поля";
-						exit();
-				}
-			}
-		}
-	}
-
-	class SaveToTxt {
-
-		public function text_save($name, $lastname, $email, $ticket) {
-
-			$currentDate = date("d_m_Y");
-
-			$baseFile = fopen("registration_$currentDate.txt", "a+");
-
-			if (strpos(file_get_contents("registration_$currentDate.txt"), $email)){
-				echo $errorMessage = "Этот email уже зарегистрирован!";
-				exit();
-			} else {
-				fwrite($baseFile, $name.' '.$lastname.' '.$email.' '.$ticket.'; ');
-				echo $successMessage = "Вы успешно зарегистрированы!";
-				exit();
-			}
-			fclose($baseFile);
-		}
-	}
-
-	class SaveToDB {
-
-		public function db_save($name, $lastname, $email, $ticket) {
-
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-
-			$conn = mysqli_connect($servername, $username, $password);
-
-			if (!$conn) {
-				die("Connection failed: " . mysqli_connect_error());
-			}
-
-			$createDB = mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS reg_form_db");
-
-			$connDB = mysqli_select_db($conn, "reg_form_db");
-
-			$table = "CREATE TABLE IF NOT EXISTS visitors_tickets (
-				id INT(6) NOT NULL AUTO_INCREMENT, 
-				name VARCHAR(30) NOT NULL DEFAULT '',
-				lastname VARCHAR(30) NOT NULL DEFAULT '',
-				email VARCHAR(50) NOT NULL DEFAULT '',
-				ticket_type VARCHAR(15),
-				reg_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-				PRIMARY KEY (id)
-			)";
-
-			$createTable = mysqli_query($conn, $table);
-
-			$checkEmail = mysqli_fetch_array(mysqli_query($conn, "SELECT * FROM visitors_tickets WHERE email = '$email'"), MYSQLI_NUM);
-
-			if (!empty($checkEmail)) {
-				echo $errorMessage = "Этот email уже зарегистрирован!";
-				exit();
-			}else{
-				mysqli_query($conn, "INSERT INTO visitors_tickets (name, lastname, email, ticket_type)
-									VALUES ('$name', '$lastname', '$email', '$ticket')");
-				echo $successMessage = "Вы успешно зарегистрированы!";
-				exit();
-			}
-		}
-	}
-
-	class SaveToXML {
-
-		public function xml_save($name, $lastname, $email, $ticket) {
-
-			$currentDate = date("d_m_Y");
-
-			$baseFile = "registration_".$currentDate.".xml";
-
-			$sxml = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?>'.'<visitors></visitors>');
-
-			if (!file_exists($baseFile)) {
-				$sxml->asXML($baseFile);
-			}
-
-			if (strpos(file_get_contents($baseFile), $email)){
-				echo $errorMessage = "Этот email уже зарегистрирован!";
-				exit();
-			} else {
-				$sxml = simplexml_load_file($baseFile);
-				$visitor = $sxml->addChild("visitor");
-				$visitor->addChild("firstname", $name);
-				$visitor->addChild("lastname", $lastname);
-				$visitor->addChild("email", $email);
-				$visitor->addChild("ticket", $ticket);
-				$sxml->asXML($baseFile);
-				echo $successMessage = "Вы успешно зарегистрированы!";
-				exit();
 			}
 		}
 	}
