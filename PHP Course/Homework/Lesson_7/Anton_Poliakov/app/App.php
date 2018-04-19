acbce547de38719471326294efcfc9c108141467
Anton Polyakov <butkamone@gmail.com>
2018-04-19 12:50:39 +0300

    Anton Polaikov PHP Lesson 7

---
+++ PHP Course/Homework/Lesson_7/Anton_Poliakov/app/App.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Anton_Poliakov\App;
+
+class App
+{
+    public $data;
+
+    public $method;
+
+    public function __construct()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->data = $_POST;
+    }
+
+    public function bootstrap()
+    {
+        return $this->request($this->data);
+    }
+
+    public function request($data)
+    {
+        $store = new Store;
+
+        if ($this->method === 'POST') {
+
+            switch (config('database')) {
+                case 'xml':
+                    return $store->storexml(
+                        filterFormData($data)
+                    );
+                    break;
+                case 'mysql':
+                    return $store->storemysql(
+                        filterFormData($data)
+                    );
+                    break;
+                case 'txt':
+                    return $store->storetxt(
+                        filterFormData($data)
+                    );
+                default;
+            }
+        }
+    }
+}
